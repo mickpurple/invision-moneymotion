5946c067401609013c2534488c144f7f28dd8741
Kartikey Soni <themickpurple@gmail.com>
2024-12-13 20:38:44 +0530

    a

--- applications/blog/dev/jslang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang = array(
-	'blog_category_confirm_delete'  => "Are you sure you want to delete this category?",
-	'blog_category_confirm_delete_desc' => "Existing entries in this category will be updated so that they are no longer in a category.",
-	'manage_cats_new_row' => "Add New Category",
-	'entry_cat_order_saved'	=> "Category order saved",
-);
--- applications/calendar/dev/js/front/templates/ips.calendar.templates.js
+++
@@ -1,9 +0,0 @@
-/**
- * Invision Community
- * (c) Invision Power Services, Inc. - https://www.invisioncommunity.com
- */
-
-/* REMINDER BUTTON LOADING */
-ips.templates.set('calendar.reminder.loading', " \
-<div class='ipsLoading ipsLoading_tiny'></div>\
-");
