0b3343a294937c9dd1bfccf69a96ce49580d525f
root <themickpurple@gmail.com>
2024-12-15 06:31:56 +0000

    created app

---
+++ applications/moneymotiongateway/Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		moneymotion_gateway Application Class
+ * @author		<a href='https://www.rolistudio.online/'>mickpurple | ROLI</a>
+ * @copyright	(c) 2024 mickpurple | ROLI
+ * @package		Invision Community
+ * @subpackage	moneymotion_gateway
+ * @since		15 Dec 2024
+ * @version		
+ */
+ 
+namespace IPS\moneymotiongateway;
+
+/**
+ * moneymotion_gateway Application Class
+ */
+class _Application extends \IPS\Application
+{
+	
+}
