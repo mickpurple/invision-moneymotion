110d81fe91f053976dc4800d2e5c1c57bdf7c182
root <themickpurple@gmail.com>
2024-12-15 06:13:15 +0000

    install

---
+++ datastore/cms_fieldids_1.4e52a1ea3c.php
@@ -0,0 +1,5 @@
+<?php
+
+return <<<'VALUE'
+{"1":1,"2":2}
+VALUE;
---
+++ datastore/loginMethods.4e52a1ea3c.php
@@ -0,0 +1,5 @@
+<?php
+
+return <<<'VALUE'
+{"1":{"login_id":1,"login_classname":"IPS\\Login\\Handler\\Standard","login_order":1,"login_acp":1,"login_settings":"{\"auth_types\":\"2\"}","login_enabled":1,"login_register":1,"login_front":1},"2":{"login_id":2,"login_classname":"IPS\\convert\\Login","login_order":2,"login_acp":1,"login_settings":"{\"auth_types\":2}","login_enabled":1,"login_register":0,"login_front":1}}
+VALUE;
