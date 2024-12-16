7d0324b2ecf9b8de70c8094d46e15bb557f4169d
root <themickpurple@gmail.com>
2024-12-16 19:49:24 +0000

    upd

---
+++ conf_global.php
@@ -0,0 +1,18 @@
+<?php
+
+$INFO = array (
+  'sql_host' => 'localhost',
+  'sql_database' => 'DB',
+  'sql_user' => 'mick',
+  'sql_pass' => 'hello123',
+  'sql_port' => 3306,
+  'sql_socket' => '/var/run/mysqld/mysqld.sock',
+  'sql_tbl_prefix' => '',
+  'sql_utf8mb4' => true,
+  'board_start' => 1734378455,
+  'installed' => true,
+  'base_url' => 'http://159.69.17.85/invision-moneymotion/',
+  'guest_group' => 2,
+  'member_group' => 3,
+  'admin_group' => 4,
+);
---
+++ plugins/hooks.php
@@ -0,0 +1,212 @@
+<?php
+return array (
+  '\\IPS\\Http\\Url\\Internal' => 
+  array (
+    15 => 
+    array (
+      'file' => 'applications/cms/hooks/InternalUrl.php',
+      'class' => 'cms_hook_InternalUrl',
+    ),
+  ),
+  '\\IPS\\forums\\api\\forums' => 
+  array (
+    28 => 
+    array (
+      'file' => 'applications/cms/hooks/apiForum.php',
+      'class' => 'cms_hook_apiForum',
+    ),
+    14 => 
+    array (
+      'file' => 'applications/downloads/hooks/apiForum.php',
+      'class' => 'downloads_hook_apiForum',
+    ),
+  ),
+  '\\IPS\\Output' => 
+  array (
+    27 => 
+    array (
+      'file' => 'applications/cms/hooks/Output.php',
+      'class' => 'cms_hook_Output',
+    ),
+  ),
+  '\\IPS\\Http\\Url\\Friendly' => 
+  array (
+    26 => 
+    array (
+      'file' => 'applications/cms/hooks/FriendlyUrl.php',
+      'class' => 'cms_hook_FriendlyUrl',
+    ),
+  ),
+  '\\IPS\\forums\\Topic' => 
+  array (
+    25 => 
+    array (
+      'file' => 'applications/cms/hooks/Topic.php',
+      'class' => 'cms_hook_Topic',
+    ),
+  ),
+  '\\IPS\\Widget' => 
+  array (
+    24 => 
+    array (
+      'file' => 'applications/cms/hooks/Widget.php',
+      'class' => 'cms_hook_Widget',
+    ),
+  ),
+  '\\IPS\\Data\\Store\\Database' => 
+  array (
+    23 => 
+    array (
+      'file' => 'applications/cms/hooks/Database.php',
+      'class' => 'cms_hook_Database',
+    ),
+    1 => 
+    array (
+      'file' => 'applications/nexus/hooks/clientAreaLinkLoad.php',
+      'class' => 'nexus_hook_clientAreaLinkLoad',
+    ),
+    4 => 
+    array (
+      'file' => 'applications/nexus/hooks/Database.php',
+      'class' => 'nexus_hook_Database',
+    ),
+  ),
+  '\\IPS\\Lang' => 
+  array (
+    22 => 
+    array (
+      'file' => 'applications/cms/hooks/Lang.php',
+      'class' => 'cms_hook_Lang',
+    ),
+  ),
+  '\\IPS\\Theme\\class_forums_front_topics' => 
+  array (
+    21 => 
+    array (
+      'file' => 'applications/cms/hooks/topics.php',
+      'class' => 'cms_hook_topics',
+    ),
+  ),
+  '\\IPS\\forums\\Topic\\Post' => 
+  array (
+    20 => 
+    array (
+      'file' => 'applications/cms/hooks/Post.php',
+      'class' => 'cms_hook_Post',
+    ),
+  ),
+  '\\IPS\\Text\\Parser' => 
+  array (
+    19 => 
+    array (
+      'file' => 'applications/cms/hooks/Parser.php',
+      'class' => 'cms_hook_Parser',
+    ),
+  ),
+  '\\IPS\\Http\\Url' => 
+  array (
+    18 => 
+    array (
+      'file' => 'applications/cms/hooks/Url.php',
+      'class' => 'cms_hook_Url',
+    ),
+  ),
+  '\\IPS\\forums\\modules\\admin\\forums\\forums' => 
+  array (
+    17 => 
+    array (
+      'file' => 'applications/cms/hooks/forums.php',
+      'class' => 'cms_hook_forums',
+    ),
+    13 => 
+    array (
+      'file' => 'applications/downloads/hooks/Forum.php',
+      'class' => 'downloads_hook_Forum',
+    ),
+  ),
+  '\\IPS\\forums\\Forum' => 
+  array (
+    16 => 
+    array (
+      'file' => 'applications/cms/hooks/Forum.php',
+      'class' => 'cms_hook_Forum',
+    ),
+    10 => 
+    array (
+      'file' => 'applications/downloads/hooks/Forums.php',
+      'class' => 'downloads_hook_Forums',
+    ),
+  ),
+  '\\IPS\\core\\modules\\front\\modcp\\modcp' => 
+  array (
+    12 => 
+    array (
+      'file' => 'applications/downloads/hooks/frontModcp.php',
+      'class' => 'downloads_hook_frontModcp',
+    ),
+  ),
+  '\\IPS\\nexus\\Package\\Item' => 
+  array (
+    11 => 
+    array (
+      'file' => 'applications/downloads/hooks/nexusPackage.php',
+      'class' => 'downloads_hook_nexusPackage',
+    ),
+  ),
+  '\\IPS\\Dispatcher\\Front' => 
+  array (
+    9 => 
+    array (
+      'file' => 'applications/convert/hooks/FrontDispatcher.php',
+      'class' => 'convert_hook_FrontDispatcher',
+    ),
+  ),
+  '\\IPS\\Db' => 
+  array (
+    8 => 
+    array (
+      'file' => 'applications/convert/hooks/Db.php',
+      'class' => 'convert_hook_Db',
+    ),
+  ),
+  '\\IPS\\Text\\LegacyParser' => 
+  array (
+    7 => 
+    array (
+      'file' => 'applications/convert/hooks/LegacyParser.php',
+      'class' => 'convert_hook_LegacyParser',
+    ),
+  ),
+  '\\IPS\\Login\\Handler' => 
+  array (
+    6 => 
+    array (
+      'file' => 'applications/convert/hooks/LoginHandler.php',
+      'class' => 'convert_hook_LoginHandler',
+    ),
+  ),
+  '\\IPS\\Dispatcher\\Admin' => 
+  array (
+    5 => 
+    array (
+      'file' => 'applications/nexus/hooks/DispatcherAdmin.php',
+      'class' => 'nexus_hook_DispatcherAdmin',
+    ),
+  ),
+  '\\IPS\\Theme\\class_core_front_global' => 
+  array (
+    3 => 
+    array (
+      'file' => 'applications/nexus/hooks/clientAreaLink.php',
+      'class' => 'nexus_hook_clientAreaLink',
+    ),
+  ),
+  '\\IPS\\core\\modules\\front\\system\\register' => 
+  array (
+    2 => 
+    array (
+      'file' => 'applications/nexus/hooks/register.php',
+      'class' => 'nexus_hook_register',
+    ),
+  ),
+);
