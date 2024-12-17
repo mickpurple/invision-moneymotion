beb3db86209968ab00f5435c3df3fc3cace47e60
root <themickpurple@gmail.com>
2024-12-17 04:50:29 +0000

    fresh

---
+++ datastore/acpNotificationIds.4e52a1ea3c.php
@@ -0,0 +1,5 @@
+<?php
+
+return <<<'VALUE'
+{"1":{"dynamic":[],"optional":[],"normal":{"1":1},"high":[],"critical":[]}}
+VALUE;
---
+++ datastore/acpNotifications.4e52a1ea3c.php
@@ -0,0 +1,5 @@
+<?php
+
+return <<<'VALUE'
+[{"id":1,"app":"core","ext":"ConfigurationError","sent":1734378584,"extra":"dangerousFunctions"}]
+VALUE;
